48b8785901139adc6aef9e89c14b06e0e76c80ed
MarcoArdit-FSD <75762182+MarcoArdit-FSD@users.noreply.github.com>
2021-03-02 19:03:33 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -1,44 +1,20 @@
-"use strict";
-let increaseValue = 1;
-let count = 0;
+let add = document.getElementById('increase');
+let less = document.getElementById('decrease');
+
+let int = document.getElementById('zero');
+let integer = 0;
 
 
-let btnHandler = (event) => {
-  event.target.name === "increase" ?
-  count+=increaseValue:
-  count-=increaseValue;
+if(add){
+  add.addEventListener('click', function() {
+    integer += 1;
+    int.innerHTML = integer;
+  })};
+  
 
-  result.textContent = count;
-};
+if(less){
+less.addEventListener('click', function() {
+  integer -= 1;
+  int.innerHTML = integer;
+})};
 
-
-let checkHandler = (event) => {
-  let labelText = "";
-
-  if(event.target.checked){
-    increaseValue = 10;
-    labelText = "Ritorna a -1 / +1";
-  } else {
-    increaseValue = 1;
-    labelText = "Trasforma in -10 / +10";
-  }
-
-
-  buttonList.forEach(btn => {
-      btn.name === "increase" ?
-      btn.textContent = `+${increaseValue}` :
-      btn.textContent = `-${increaseValue}`
-    });
-
-  label.textContent = labelText;
-}
-
-const buttonList = document.querySelectorAll("button");
-const result = document.querySelector("span");
-const label = document.querySelector("label");
-const checkBox = document.querySelector("input");
-
-buttonList.forEach((btn) => {
-  btn.addEventListener("click", btnHandler)});
-
-checkBox.addEventListener("change", checkHandler);
